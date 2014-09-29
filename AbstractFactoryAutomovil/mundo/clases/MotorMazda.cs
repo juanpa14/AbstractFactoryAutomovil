b722ea04091fb70a82a421658aef2d5f4f2ce6bb
Juan <juan.rr.08@gmail.com>
2014-09-29 11:13:58 -0500

    Correción

--- AbstractFactoryAutomovil/mundo/clases/MotorMazda.cs
+++ AbstractFactoryAutomovil/mundo/clases/MotorMazda.cs
@@ -10,7 +10,7 @@ namespace AbstractFactoryAutomovil.mundo.clases
     {
         public string darNombre()
         {
-            return "Carroceria Mazda";
+            return "Motor Mazda";
         }
     }
 }
